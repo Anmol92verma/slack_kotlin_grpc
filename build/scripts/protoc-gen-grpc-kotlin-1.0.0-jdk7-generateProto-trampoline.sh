c33ece3dae44e64212762382992d6039dc513bf3
Anmol Verma <anmol.verma4@gmail.com>
2021-11-27 17:41:32 +0530

    remove build and other repos

--- build/scripts/protoc-gen-grpc-kotlin-1.0.0-jdk7-generateProto-trampoline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec '/Users/anmolverma/Library/Java/JavaVirtualMachines/corretto-15.0.2/Contents/Home/bin/java' -jar '/Users/anmolverma/.gradle/caches/modules-2/files-2.1/io.grpc/protoc-gen-grpc-kotlin/1.0.0/d6c8cbe29c60b202165593be5244ce43d37378ac/protoc-gen-grpc-kotlin-1.0.0-jdk7.jar' "$@"
